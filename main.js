ef5ffaf97ae7c2f42c73075149aacf15954fca26
Tanja Kocheva <tanjakocheva95@gmail.com>
2021-02-07 22:29:33 +0100

    commit

---
+++ main.js
@@ -0,0 +1,110 @@
+// Prva zadaca 
+
+// let num = prompt('Please enter a number: '); 
+ 
+//   if (num % 2 == 0) {
+//     console.log("The number is even.");
+//   } else {
+//     console.log("The number is odd.");
+//   }
+
+
+//   Vtora zadaca 
+
+// for (let i = 10; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 2 == 0){ 
+//         console.log(i + " ");
+       
+//     }
+// }
+
+
+
+// // Treta zadaca 
+
+// let num1 = parseInt(prompt('Please enter a number: '));
+// let num2 = parseInt(prompt('Please enter a number: ')) ;
+// let num3 = parseInt(prompt('Please enter a number: ')) ;
+// let isPrime = true;
+
+// console.log (`Number 1: ${num1}`);
+// console.log (`Number 2: ${num2}`);
+// console.log (`Number 3: ${num3}`);
+
+// if (num1 > num2 && num1 > num3 ) {
+//     console.log (`The largest number is ${num1}.`);
+// } else if (num2 > num1 && num2 > num3) {
+//     console.log (`The largest number is ${num2}.`);
+// }else {
+//     console.log (`The largest number is ${num3}.`);
+// }
+
+// if (num1 < num2 && num1 < num3) {
+//     console.log (`The smallest number is ${num1}.`);
+// } else if (num2 < num1 && num2 < num3) {
+//     console.log (`The smallest number is ${num2}.`);
+// }else {
+//     console.log (`The smallest number is ${num3}.`);
+// }
+
+
+// if (num1 === 1 ) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+// else if (num1 > 1) {
+    
+//     for (let i = 2; i < num1; i++) {
+//         if (num1 % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(`${num1} is a prime number`);
+//     } else {
+//         console.log(`${num1} is not a prime number`);
+//     }
+// }
+// else {
+//     console.log("The number is not a prime number.");
+// }
+
+// if (num2 === 1 ) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+// else if (num2 > 1) {
+//     for (let i = 2; i < num2; i++) {
+//         if (num2 % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(`${num2} is a prime number`);
+//     } else {
+//         console.log(`${num2} is not a prime number`);
+//     }
+// }
+// else {
+//     console.log("The number is not a prime number.");
+// }
+
+// if (num3 === 1 ) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+// else if (num3 > 1) {
+//     for (let i = 2; i < num3; i++) {
+//         if (num3 % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(`${num3} is a prime number`);
+//     } else {
+//         console.log(`${num3} is not a prime number`);
+//     }
+// }
+// else {
+//     console.log("The number is not a prime number.");
+// }
