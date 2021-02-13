a1d302e2c76b8ed0928a16f6a98d61368c605a1c
Tanja Kocheva <tanjakocheva95@gmail.com>
2021-02-13 14:58:35 +0100

    commit2

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-const niza = [
-    ['Antonio', true, 5], ['Marija', false],['Darko', false], ['Angel', true, 3], ['Nadica', true, 2]
-];
+const voters = [['Antonio', true, 5], ['Marija', false],['Darko', false], ['Angel', true, 3], ['Nadica', true, 2]];
 
-const ime = prompt("Please enter a name: ");
+const enterName = prompt("Please enter a name: ");
 
 let doesNameExist = false;
 
-for (let i = 0; i < niza.length; i++) {
-    if (niza[i][0] === ime) {
-        if (niza[i][1] === true) {
-            console.log(`${niza[i][0]} | Voted | ${niza[i][2]}`)
+for (let i = 0; i < voters.length; i++) {
+    if (voters[i][0] === enterName) {
+        if (voters[i][1] === true) {
+            console.log(`${voters[i][0]} | Voted | ${voters[i][2]}`)
             doesNameExist = true;
         } else {
-            console.log(`${niza[i][0]} | Didn't vote`);
+            console.log(`${voters[i][0]} | Didn't vote`);
             doesNameExist = true;
         }    
-    } else if (doesNameExist === false && niza[i] === niza[niza.length - 1]) {
-        console.log('The name is not found.');
+    } else if (doesNameExist === false && voters[i] === voters[voters.length - 1]) {
+        console.log('The name was not found.');
     }
 }
